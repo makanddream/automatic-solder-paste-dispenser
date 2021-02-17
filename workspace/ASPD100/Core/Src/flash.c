a3309af53e4fce5dbc4490a680b9466866b488b1
diy-maker <60671162+diy-maker@users.noreply.github.com>
2021-02-17 13:24:15 +0100

    Fix function to save data into the flash

--- workspace/ASPD100/Core/Src/flash.c
+++ workspace/ASPD100/Core/Src/flash.c
@@ -49,6 +49,9 @@ uint8_t flash_save_buffer(const uint8_t *buffer, const uint16_t length) {
 
 	uint32_t PAGEError = 0, FirstPage = 0, BankNumber = 0;
 
+	/* Unlock the Flash to enable the flash control register access *************/
+	HAL_FLASH_Unlock();
+
 	/* Get the 1st page to erase */
 	FirstPage = GetPage((uint32_t)settings_page);
 
@@ -63,26 +66,36 @@ uint8_t flash_save_buffer(const uint8_t *buffer, const uint16_t length) {
 	EraseInitStruct.NbPages = 1;
 
 	__HAL_FLASH_CLEAR_FLAG(FLASH_FLAG_EOP | FLASH_FLAG_WRPERR | FLASH_FLAG_PGAERR | FLASH_FLAG_BSY);
+	/* Unlock the Flash to enable the flash control register access *************/
 	HAL_FLASH_Unlock();
 	HAL_Delay(1);
 
+	// Erase the page of flash (2048 bytes on this stm32)
 	if (HAL_FLASHEx_Erase(&EraseInitStruct, &PAGEError) != HAL_OK)
 	{
 		/*Error occurred while page erase.*/
 		return HAL_FLASH_GetError ();
 	}
 
-	//^ Erase the page of flash (1024 bytes on this stm32)
-	// erased the chunk
-	// now we program it
-    uint64_t *data = (uint64_t*) buffer;
+    /* Unlock the Flash to enable the flash control register access *************/
 	HAL_FLASH_Unlock();
 
-	for (uint8_t i = 0; i < (length / 2); i++) {
-		HAL_FLASH_Program(FLASH_TYPEPROGRAM_DOUBLEWORD, (uint32_t)&settings_page[i], data[i]);
+    uint64_t *data = (uint64_t*) buffer;
+
+	/*
+	 * Length is divide by 8 because we program double word.
+	 * Word = 4 byte, Double Word = 8 byte.
+	 */
+	for (uint8_t i = 0; i < (length / 8); i++) {
+		HAL_FLASH_Program(FLASH_TYPEPROGRAM_DOUBLEWORD, (uint32_t)&settings_page[i + (3*i)], data[i]);
+		FLASH_WaitForLastOperation(1000);
 	}
 
+	/* Lock the Flash to disable the flash control register access (recommended
+	 * to protect the FLASH memory against possible unwanted operation) ********
+	 */
 	HAL_FLASH_Lock();
+
 	return 1;
 }
 
